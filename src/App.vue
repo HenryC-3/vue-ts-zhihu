854e25814ae2e4177ae96bc86f3c71ed27dff8ba
HenryC-3 <2324375439@qq.com>
2021-03-09 14:49:08 +0800

    feat(组件): ColumnList 组件初始化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
-  <div></div>
+  <column-list :list="data"></column-list>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import ColumnList from "./components/ColumnList.vue";
+
+const testData = [
+  {
+    id: 1,
+    title: "test1的专栏",
+    description: "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar:
+      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100"
+  },
+  {
+    id: 2,
+    title: "test2的专栏",
+    description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar:
+      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100"
+  }
+];
 
 export default defineComponent({
   name: "App",
-  components: {}
+  components: { ColumnList },
+  setup() {
+    const data = testData;
+    return { data };
+  }
 });
 </script>
 
