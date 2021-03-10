e643e90f99682de88d4ccf5ac8046d90739123fe
HenryC-3 <2324375439@qq.com>
2021-03-10 15:21:54 +0800

    feat(组件): dropdown 组件点击组件外区域时,关闭下拉列表

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="dropdown">
+  <!-- 使用 ref 获取 DOM 元素 -->
+  <div class="dropdown" ref="dropdownRef">
     <a
       href="#"
       class="btn btn-outline-light my-2 dropdown-toggle"
@@ -12,8 +13,9 @@
     </ul>
   </div>
 </template>
-<script>
+<script lang="ts">
 import { ref } from "@vue/reactivity";
+import { onMounted, onUnmounted } from "@vue/runtime-core";
 export default {
   name: "Dropdown",
   props: {
@@ -27,9 +29,24 @@ export default {
     const toggleOpen = () => {
       isOpen.value = !isOpen.value;
     };
+    // 获取 DOM 元素
+    const dropdownRef = ref<null | HTMLElement>(null);
+    const handler = (e: MouseEvent) => {
+      if (!dropdownRef.value?.contains(e.target) && isOpen.value === true) {
+        isOpen.value = false;
+      }
+    };
+    onMounted(() => {
+      document.addEventListener("click", handler);
+    });
+    onUnmounted(() => {
+      document.removeEventListener("click", handler);
+    });
     return {
       toggleOpen,
-      isOpen
+      isOpen,
+      // 获取 DOM 元素
+      dropdownRef
     };
   }
 };
