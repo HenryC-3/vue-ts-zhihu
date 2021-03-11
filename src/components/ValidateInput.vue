e07b6d42f7ce1ff9df5707c3cf20bc602d7e09c3
HenryC-3 <2324375439@qq.com>
2021-03-11 10:47:33 +0800

    feat(组件): vlidateInput 组件初始化

---
+++ src/components/ValidateInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <input type="email" class="form-control" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    //
+  }
+});
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div></div>
+  <form>
+    <div class="mb-3">
+      <label class="form-label">邮箱</label>
+      <validate-input></validate-input>
+      <div id="emailHelp" class="form-text">
+        请输入邮箱
+      </div>
+    </div>
+    <div class="mb-3">
+      <label class="form-label">密码</label>
+      <validate-input></validate-input>
+      <div id="emailHelp" class="form-text">
+        请输入密码
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">登录</button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ValidateInput from "../components/ValidateInput.vue";
+
+export default defineComponent({
+  name: "Login",
+  components: { ValidateInput },
+  setup() {
+    //
+  }
+});
+</script>
