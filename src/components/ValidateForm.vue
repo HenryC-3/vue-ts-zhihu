53ddb13b07b5bd4ae04f868cc454569ef2953166
HenryC-3 <2324375439@qq.com>
2021-03-12 13:38:55 +0800

    chore: 改进点击区域

--- src/components/ValidateForm.vue
+++ src/components/ValidateForm.vue
@@ -6,6 +6,7 @@
     </div>
     <!-- 点击提交时触发 form-submit 事件 -->
     <div @click.prevent="submitForm">
+      <!-- IMPR:点击触发区域为默认宽度 100% 限定点击触发区域为 slot 渲染区域大小-->
       <slot name="submit">
         <!-- NOTE：在 slot 内容渲染默认内容 -->
         <button type="submit" class="btn btn-primary">登录</button>
@@ -21,7 +22,7 @@ import { defineComponent } from "@vue/runtime-core";
 type validateFunc = () => boolean;
 const funcArr: validateFunc[] = [];
 export default defineComponent({
-  // VIEW：为什么之前没用 defineComponent 也没事？
+  // VIEW：为什么之前没用 defineComponent 也
   emits: ["form-submit"],
   setup(props, context) {
     const handler: Handler = (func: validateFunc) => {
