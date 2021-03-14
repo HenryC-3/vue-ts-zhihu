f5df4011055c8933658ddb7c1115de7e81d6a974
HenryC-3 <2324375439@qq.com>
2021-03-14 11:09:14 +0800

    feat(视图): createPost 视图初始化

---
+++ src/views/CreatePost.vue
@@ -0,0 +1,20 @@
+<template>
+  <h3>新建文章</h3>
+  <p>文章标题：</p>
+  <validate-input></validate-input>
+  <p>文章内容：</p>
+  <validate-input></validate-input>
+  <button type="submit" class="btn btn-primary btn-outline-light">提交</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import ValidateInput from "../components/ValidateInput.vue";
+export default defineComponent({
+  name: "CreatePost",
+  components: { ValidateInput },
+  setup() {
+    //
+  }
+});
+</script>
