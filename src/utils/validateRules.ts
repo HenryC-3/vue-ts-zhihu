34594e6aa34f290587e4b0228b9cf8ac8b549faf
HenryC-3 <2324375439@qq.com>
2021-03-14 14:09:40 +0800

    feat(组件): 添加验证规则

--- src/utils/validateRules.ts
+++ src/utils/validateRules.ts
@@ -4,3 +4,11 @@ export const emailRule: RulesProp = [
   { type: "required", message: "邮箱不能为空" },
   { type: "email", message: "邮箱格式错误" }
 ];
+
+export const postTitleRule: RulesProp = [
+  { type: "required", message: "文章标题不能为空" }
+];
+
+export const postContentRule: RulesProp = [
+  { type: "required", message: "文章内容不能为空" }
+];
--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,12 +1,16 @@
 <template>
   <h3>新建文章</h3>
   <p>文章标题：</p>
-  <validate-input placeholder="请输入文章标题"></validate-input>
+  <validate-input
+    placeholder="请输入文章标题"
+    :rules="postTitleRule"
+  ></validate-input>
   <p>文章内容：</p>
   <validate-input
     placeholder="请输入文章详情"
     :tag="textarea"
     rows="10"
+    :rules="postContentRule"
   ></validate-input>
   <button type="submit" class="btn btn-primary btn-outline-light">提交</button>
 </template>
@@ -14,13 +18,16 @@
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
 import ValidateInput from "../components/ValidateInput.vue";
+import { postTitleRule, postContentRule } from "../utils/validateRules";
 export default defineComponent({
   name: "CreatePost",
   components: { ValidateInput },
   setup() {
     const textarea = "textarea";
     return {
-      textarea
+      textarea,
+      postTitleRule,
+      postContentRule
     };
   }
 });
