b2f42c46d153e9beccdca5d4e5b079a0e03bb688
HenryC-3 <2324375439@qq.com>
2021-03-16 15:08:19 +0800

    feat(视图):  Home 视图获取真实图片替换 mock 图片

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,6 +1,7 @@
 import { GlobalStore, PostProps } from "@/types/types";
 import { createStore } from "vuex";
 import { testData, testPostData } from "./mockData";
+import axios from "axios";
 
 export const store = createStore<GlobalStore>({
   state: {
@@ -14,6 +15,16 @@ export const store = createStore<GlobalStore>({
     },
     createPost(state, post: PostProps) {
       state.posts.push(post);
+    },
+    fetchColumns(state, rowData) {
+      state.columns = rowData.data.list;
+    }
+  },
+  actions: {
+    fetchColumns(context) {
+      axios.get("columns").then(res => {
+        context.commit("fetchColumns", res.data);
+      });
     }
   },
   getters: {
