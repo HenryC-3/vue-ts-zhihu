974b9023c7ac5bcbdcb4239d18888fbdc0baa461
HenryC-3 <2324375439@qq.com>
2021-03-16 12:39:17 +0800

    chore: 配置环境变量, 调用后端 API

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { store } from "./utils/store";
 import { router } from "./utils/router";
+import axios from "axios";
+
+const ICODE = process.env.VUE_APP_ICODE;
+
+axios.get(`http://apis.imooc.com/api/columns?icode=${ICODE}`).then(res => {
+  console.log(res.data);
+});
 
 const app = createApp(App);
-
 app.use(router);
 app.use(store);
 app.mount("#app");
