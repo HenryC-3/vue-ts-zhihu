c1986b242d5069b5964a7dbd92dda6d33aeebea9
HenryC-3 <2324375439@qq.com>
2021-03-16 09:42:37 +0800

    feat(路由): 用户未登录时,访问其他页面自动跳转到 /login

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -3,6 +3,7 @@ import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import ColumnDetail from "../views/ColumnDetail.vue";
 import CreatePost from "../views/CreatePost.vue";
+import { store } from "../utils/store";
 
 const routerHistory = createWebHistory();
 export const router = createRouter({
@@ -29,4 +30,12 @@ export const router = createRouter({
       component: CreatePost
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (!store.state.user.isLogin && to.name !== "login") {
+    next({ name: "login" });
+  } else {
+    next();
+  }
 });
