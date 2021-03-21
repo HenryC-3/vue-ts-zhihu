90c9469729b33ff7913b7e827ba6ecd750382183
HenryC-3 <2324375439@qq.com>
2021-03-21 13:40:09 +0800

    feat(Message 组件): teleport

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,24 +1,27 @@
 <template>
-  <div
-    v-if="isVisible"
-    class="alert global-msg"
-    :class="{ 'alert-success': !error.status, 'alert-danger': error.status }"
-    role="alert"
-  >
-    <span>{{ error.message }}</span>
-    <button
-      type="button"
-      class="btn-close"
-      @click.prevent="hide"
-      aria-label="Close"
-    ></button>
-  </div>
+  <teleport to="#message">
+    <div
+      v-if="isVisible"
+      class="alert global-msg"
+      :class="{ 'alert-success': !error.status, 'alert-danger': error.status }"
+      role="alert"
+    >
+      <span>{{ error.message }}</span>
+      <button
+        type="button"
+        class="btn-close"
+        @click.prevent="hide"
+        aria-label="Close"
+      ></button>
+    </div>
+  </teleport>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, PropType, ref, watch } from "vue";
 import { ErrorProps } from "@/types/types";
 import { useStore } from "vuex";
+import useTeleportDom from "../hooks/useTeleportDom";
 
 export default defineComponent({
   name: "Message",
@@ -31,6 +34,7 @@ export default defineComponent({
   setup() {
     const store = useStore();
     const isVisible = ref(false);
+    useTeleportDom("div", "message");
     watch(
       computed(() => {
         return store.state.error.message;
