cece2b28b8bf62b93938d92df3b79bd9f5bcd387
HenryC-3 <2324375439@qq.com>
2021-04-16 14:28:39 +0800

    feat(PostDetail)：修改文章功能，添加修改和删除按钮

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <article class="w-75 mx-auto mb-5 pb-3" v-if="currentPost">
+      <!-- 头图 -->
       <img
         :src="currentImageUrl"
         alt="currentPost.title"
         class="rounded-lg img-fluid my-4"
         v-if="currentImageUrl"
       />
+      <!-- 标题 -->
       <h2 class="mb-4">{{ currentPost.title }}</h2>
       <div
         class="user-profile-component border-top border-bottom py-3 mb-5 align-items-center row g-0"
       >
         <div class="col">
+          <!-- 用户头像、描述 -->
           <user-profile
             :user="currentPost.author"
             v-if="typeof currentPost.author === 'object'"
@@ -21,14 +24,20 @@
           >发表于：{{ currentPost.createdAt }}</span
         >
       </div>
+      <!-- 文章内容 -->
       <div v-html="currentHTML"></div>
+      <!-- 编辑按钮 -->
+      <div v-if="showEdit" class="d-flex justify-content-between">
+        <button class="btn btn-primary" @click="handleModify">修改</button>
+        <button class="btn btn-danger" @click="handleDelete">删除</button>
+      </div>
     </article>
   </div>
 </template>
 
 <script lang="ts">
 import { PostProps, AvatarProps } from "../types/types";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { computed, onMounted } from "@vue/runtime-core";
 import { useStore } from "vuex";
 import UserProfile from "../components/UserProfile.vue";
@@ -38,6 +47,7 @@ export default {
   components: { UserProfile },
   setup() {
     const route = useRoute();
+    const router = useRouter();
     const store = useStore();
     const md = new markdownIt();
     onMounted(() => {
@@ -64,10 +74,29 @@ export default {
       }
     });
 
+    const showEdit = computed(() => {
+      // 未登录情况下，user.column 一定为空
+      return (
+        store.state.user.column ===
+        store.state.posts.data[route.params.id as string].column
+      );
+    });
+
+    const handleModify = () => {
+      router.push({ path: "/create", query: { postId: route.params.id } });
+    };
+
+    const handleDelete = () => {
+      //
+    };
+
     return {
       currentPost,
       currentImageUrl,
-      currentHTML
+      currentHTML,
+      showEdit,
+      handleModify,
+      handleDelete
     };
   }
 };
