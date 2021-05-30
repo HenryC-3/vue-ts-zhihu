86cb5e34d48231fc0b2890ec14f77d64dd7e465d
HenryC-3 <2324375439@qq.com>
2021-05-30 17:03:27 +0800

    refactor: loading 组件样式修改

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- 挂载在 div#loading 上 -->
   <teleport to="#loading">
-    <div
-      class="loading-container d-flex justify-content-center align-items-center"
-      v-if="loading"
-    >
-      <div class="spinner-border text-primary" role="status">
-        <span class="visually-hidden">Loading...</span>
+    <div v-if="loading" class="mask flex justify-center items-center">
+      <div class="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   </teleport>
@@ -29,13 +29,40 @@ export default defineComponent({
   }
 });
 </script>
-<style scoped>
-.loading-container {
-  background: rgba(255, 255, 255, 0.5);
-  top: 0;
-  bottom: 0;
-  width: 100%;
-  position: fixed;
-  z-index: 100;
+<style>
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #fff transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
